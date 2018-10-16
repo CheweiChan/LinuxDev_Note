329fb04775980b80be2fa52ef7d9be486721ac8d
Chewei <31846431+CheweiChan@users.noreply.github.com>
2018-10-16 23:40:39 +0800

    Update IOTest.c

--- RW_Drv/IOTest.c
+++ RW_Drv/IOTest.c
@@ -6,9 +6,9 @@
 #define EXAMPLE_MAJOR 60
 #define EXAMPLE_NAME "IOTest"
 MODULE_LICENSE("GPL");
-int postion=0;
-uint8_t byte[10];
-int buff_len;
+int postion=0; //每次read時Buff中的位置buff[postion]
+uint8_t byte[100];//read buffer
+int buff_len;// read時需從buffer要搬多少長度
 
 static int example_open(struct inode *inode, struct file *filp) {
     printk("<1>EXAMPLE: open\n");
@@ -19,16 +19,21 @@ static int example_close(struct inode *inode, struct file *filp) {
     printk("<1>EXAMPLE: close\n");
     return 0;
 }
-
+// read function 為一個遞迴function會一直到返回值為0 返回值為kernel read len
 static ssize_t example_read(struct file *filp, char *buf, size_t size, loff_t *f_pos) {
 size_t i;
-size_t len;
+size_t len; //當次read搬的資料長度  最大為size
 printk("<1>EXAMPLE: read  (size=%zu)buff_len=%d\n", size,buff_len);
 if(buff_len < size)
 {
 len=buff_len;
+if(len==0) return 0;
 }
-if(copy_to_user(buf,&byte[0+postion],len))
+else
+{
+len=size;
+}
+if(copy_to_user(buf,&byte[0+postion],len))//從kernel space複製資料到user space
 {  
         return -EFAULT;  
 }
@@ -37,25 +42,25 @@ for(i=0;i<len;i++)
      printk("<1>EXAMPLE: resd  (byte[%zu] = %02x)\n", i, (unsigned)byte[i]);
 }
 
-if(postion==0)
+if(postion==0)//第一次讀取從buffer位置0開始
 {
-postion+=len;
+postion+=len;//之後位置為postion+=len
 }
-else
+else//第二次讀取
 {
-if(buff_len==0)
+if(buff_len==0)//buff_len=0則讀取完畢
 {
-len=0;
+len=0;//kernel需要讀取幾個資料
 postion=0;
 }
-else
+else//buff_lem 不為0則繼續讀取
 {
 len=size;
 postion+=size;
 }
 }
-buff_len=buff_len-len;
-    return len;
+buff_len=buff_len-len;//剩餘長度=減掉這次讀出去的資料長度
+    return len;//kernel 需要讀取的長度 （最後必須返回0讀取才會結束）
 }
 
 
