d4d231348e0646cecc7b9e96fbda27db9edfdfdc
Chewei <31846431+CheweiChan@users.noreply.github.com>
2019-01-06 16:16:14 +0800

    Update RWTest.c

--- 02-RW_Drive/RWTest.c
+++ 02-RW_Drive/RWTest.c
@@ -24,56 +24,37 @@ static int example_close(struct inode *inode, struct file *filp) {
     printk("<1>EXAMPLE: close\n");
     return 0;
 }
-// 如果是用cat read 為一個遞迴function會一直到返回值為0 返回值為kernel read len
+
 static ssize_t example_read(struct file *filp, char *buf, size_t size, loff_t *f_pos) {
-size_t i;
-size_t len; //當次read搬的資料長度  最大為size
-printk("<1>EXAMPLE: read  (size=%zu)buff_len=%d\n", size,buff_len);
-if(buff_len < size)
-{
-len=buff_len;
-if(len==0)//如果buff沒資料了則重置postion以及返回0 
-{
-    buff_pos=0;
-    postion=0;
-    return 0;
-}
-}
-else
-{
-len=size;
-}
+    size_t i;
+    size_t rlen; //當次read搬的資料長度  最大為size
+    printk("<1>EXAMPLE: read  (size=%zu)buff_len=%d\n", size,buff_len);
+    if(buff_len < size)
+    {
+        rlen=buff_len;
+        if(rlen==0)//如果buff沒資料了則重置postion以及返回0 
+        {
+            buff_pos=0;
+            postion=0;
+            return 0;
+        }
+    }
+    else
+    {
+        rlen=size;
+    }
     
-if(copy_to_user(buf,&byte[0+postion],len))//從kernel space複製資料到user space
-{  
+    if(copy_to_user(buf,&byte[0+postion],rlen))//從kernel space複製資料到user space
+    {  
         return -EFAULT;  
-}
-for(i=0;i<len;i++)
-{
-     printk("<1>EXAMPLE: resd  (byte[%zu] = %02x)\n", i, (unsigned)byte[i]);
-}
-#if 0
-if(postion==0)//第一次讀取從buffer位置0開始
-{
-postion+=len;//之後位置為postion+=len
-}
-else//第二次讀取
-{
-if(buff_len==0)//buff_len=0則讀取完畢
-{
-len=0;//kernel需要讀取幾個資料
-postion=0;
-}
-else//buff_lem 不為0則繼續讀取
-{
-len=size;
-postion+=size;
-}
-}
-#endif
-postion+=size;
-buff_len=buff_len-len;//剩餘長度=減掉這次讀出去的資料長度
-return len;//kernel 需要讀取的長度 （最後必須返回0讀取才會結束）
+    }
+    for(i=0;i<rlen;i++)
+    {
+        printk("<1>EXAMPLE: resd  (byte[%zu] = %02x)\n", i, (unsigned)byte[i]);
+    }
+    postion+=size;
+    buff_len=buff_len-rlen;//剩餘長度=buff_len減掉這次讀出去的資料長度
+    return rlen;//kernel 需要讀取的長度 （若用cat read最後必須返回0讀取才會結束）
 }
 
 
@@ -84,11 +65,9 @@ static ssize_t example_write(struct file *filp, const char *buf, size_t size, lo
         if (copy_from_user(&byte[buff_len], buf++, 1) != 0) {
             break;
         }
-
         printk("<1>EXAMPLE: write  (byte[%d] = %02x)\n", buff_len,byte[buff_len]);
-
     }
-buff_pos=buff_len;
+    buff_pos=buff_len;
     return size;
 }
 
