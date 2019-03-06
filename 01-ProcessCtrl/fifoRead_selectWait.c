b8df07d55c180bac0cc2f9ec6fe7fe117215683b
Chewei <a24553120@gmail,com>
2019-03-07 01:44:27 +0800

    Update fifoRead_selectWait.c

--- 01-ProcessCtrl/fifoRead_selectWait.c
+++ 01-ProcessCtrl/fifoRead_selectWait.c
@@ -14,66 +14,57 @@ void main(int argc,char** argv)
 	char buf_r[100];
 	int  fd,a;
 	int  nread;
-	fd_set set,temp; 
+	fd_set set; 
 	struct timeval timeout;
-         FD_ZERO(&set); /*将set清零使集合中不含任何fd*/ 
-	
-	memset(buf_r,0,sizeof(buf_r));
-	
-
-	
-   
+    FD_ZERO(&set); /*将set清零使集合中不含任何fd*/ 
      // FD_SET(fd, &set); /*将fd加入set集合*/
-     // FD_CLR(fd, &set); /*将fd从set集合中清除*/ 
-     // FD_ISSET(fd, &set); /*测试fd是否在set集合中*/
+     // FD_CLR(fd, &set); /*将fd从set集合中清�?/ 
+     // FD_ISSET(fd, &set); /*测试fd是否在set集合�?/
 	while(1)
 	{
-	temp=set;
         timeout.tv_sec = 2;
         timeout.tv_usec = 0;
         memset(buf_r,0,sizeof(buf_r));
+    	if((mkfifo(FIFO,0666)<0)&&(errno!=EEXIST))
+    	printf("cannot create fifoserver\n");
+    	fd=open(FIFO,O_RDWR|O_NONBLOCK,0);
+    	if(fd==-1)
+    	{
+    		perror("open");
+    		exit(1);	
+    	}
 
-	if((mkfifo(FIFO,0666)<0)&&(errno!=EEXIST))
-	printf("cannot create fifoserver\n");
-	fd=open(FIFO,O_RDWR|O_NONBLOCK,0);
-	if(fd==-1)
-	{
-		perror("open");
-		exit(1);	
-	}
-
-	printf("Preparing for reading bytes...\n");
+	    printf("Preparing for reading bytes...\n");
 		FD_SET(fd, &set);
                 a=select(fd+1, &set, 0, 0, NULL);
 	
-	if(FD_ISSET(fd, &set))
-	{
-	printf("\n isset\n");
-	}
-	else printf("\n noset\n");
+    	if(FD_ISSET(fd, &set))
+    	{
+    	printf("\n isset\n");
+    	}
+    	else printf("\n noset\n");
 
-	if(a==0)
-	{
-	printf("\n timeout\n");
-	}
-	else if(a==-1)
-	{
-	printf("\n fail\n");
-	}
-else
-{
-printf("\ntrue\n");
-	if((nread=read(fd,buf_r,100))==-1)
-	{
-		if(errno==EAGAIN)
-		printf("no data yet\n");
-	}
-
-		printf("read %s from FIFO,%d,%d\n",buf_r,a,fd);
-unlink(FIFO);
-}      
+    	if(a==0)
+    	{
+    	printf("\n timeout\n");
+    	}
+    	else if(a==-1)
+    	{
+    	printf("\n fail\n");
+    	}
+        else
+        {
+            printf("\ntrue\n");
+    	    if((nread=read(fd,buf_r,100))==-1)
+    	    {
+    		    if(errno==EAGAIN)
+    		    printf("no data yet\n");
+    	    }
+    	printf("read %s from FIFO,%d,%d\n",buf_r,a,fd);
+        unlink(FIFO);
+        }      
         FD_CLR(fd, &set); 
-	sleep(1);
+    	sleep(1);
 	}	
 	pause(); 
 	unlink(FIFO);
