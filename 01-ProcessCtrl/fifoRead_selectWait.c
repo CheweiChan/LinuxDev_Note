f3463cbf70151f44c165efb84465633b17c743d3
chewei <cheweichan@Cheweide-MacBook-Pro.local>
2019-03-10 19:43:16 +0800

    update

--- 01-ProcessCtrl/fifoRead_selectWait.c
+++ 01-ProcessCtrl/fifoRead_selectWait.c
@@ -5,68 +5,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/select.h> 
-#include <sys/time.h>
+#include <unistd.h>
 #define FIFO "./myfifo"
 
 void main(int argc,char** argv)
 {
 	char buf_r[100];
-	int  fd,a;
-	int  nread;
-	fd_set set; 
-	struct timeval timeout;
-        FD_ZERO(&set); /*å°†setæ¸…é›¶ä½¿é›†åˆä¸­ä¸å«ä»»ä½•fd*/ 
-     // FD_SET(fd, &set); /*å°†fdåŠ å…¥seté›†åˆ*/
-     // FD_CLR(fd, &set); /*å°†fdä»Žseté›†åˆä¸­æ¸…é™?/ 
-     // FD_ISSET(fd, &set); /*æµ‹è¯•fdæ˜¯å¦åœ¨seté›†åˆä¸?/
-	while(1)
-	{
-        timeout.tv_sec = 2;
-        timeout.tv_usec = 0;
-        memset(buf_r,0,sizeof(buf_r));
-    	if((mkfifo(FIFO,0666)<0)&&(errno!=EEXIST))
-    	printf("cannot create fifoserver\n");
-    	fd=open(FIFO,O_RDWR|O_NONBLOCK,0);
-    	if(fd==-1)
-    	{
-    		perror("open");
-    		exit(1);	
-    	}
+	int  fd;
+	int  nread,result;
+	fd_set set,temp; 
+    struct timeval timeout;
+	/* create fifo*/
+    unlink(FIFO); //remove FIFO file
+	if((mkfifo(FIFO,O_CREAT|O_EXCL)<0)&&(errno!=EEXIST))
+		printf("cannot create fifoserver\n");
 
-	printf("Preparing for reading bytes...\n");
- 	FD_SET(fd, &set);
-  	a=select(fd+1, &set, 0, 0, NULL);
 	
-    	if(FD_ISSET(fd, &set))
-    	{
-    	printf("\n isset\n");
-    	}
-    	else printf("\n noset\n");
+	printf("Preparing for reading bytes...\n");
+	
+	memset(buf_r,0,sizeof(buf_r));
+	
+	/* open fifo */
+    /*Must setting RW,if setting read only will always return EOF(0),select() will always return have data*/
+	fd=open(FIFO,O_RDWR|O_NONBLOCK,0);
+    FD_ZERO(&set); /*将set清零使集合中不含任何fd*/ 
+    FD_SET(fd, &set); /*将fd加入set集合*/
 
-    	if(a==0)
-    	{
-    	printf("\n timeout\n");
-    	}
-    	else if(a==-1)
-    	{
-    	printf("\n fail\n");
-    	}
-        else
-        {
-            printf("\ntrue\n");
-    	    if((nread=read(fd,buf_r,100))==-1)
-    	    {
-    		    if(errno==EAGAIN)
-    		    printf("no data yet\n");
-    	    }
-    	printf("read %s from FIFO,%d,%d\n",buf_r,a,fd);
-        unlink(FIFO);
-        }      
-        FD_CLR(fd, &set); 
-    	sleep(1);
-	}	
-	pause(); 
-	unlink(FIFO);
+if(fd==-1)
+{
+	perror("open");
+    exit(1);	
+}
+
+while(1)
+{
+    temp=set;
+    timeout.tv_sec = 5;
+    timeout.tv_usec = 0;
+    result=select(fd+1, &temp, NULL, NULL, &timeout);//blocked until timeout
+    //result=select(fd+1, &temp, NULL, NULL, NULL/*NULL will blocked until data*/);
+    if(result == 0)
+    {
+           printf("\ntimeout\n");
+    }
+    else if(result == -1)
+    {
+           printf("\nfail\n");
+    }
+    else
+    {
+		if((nread=read(fd,buf_r,100))==-1)
+		{
+			if(errno==EAGAIN)
+				printf("no data yet\n");
+		}
+		printf("read %s from FIFO\n",buf_r);
+	    memset(buf_r,0,sizeof(buf_r));
+    }
+}	
+unlink(FIFO); //remove FIFO file
 }
 
