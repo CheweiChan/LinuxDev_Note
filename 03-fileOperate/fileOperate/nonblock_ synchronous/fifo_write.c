7ca6e080ec44218c0af5afc611bff30b2d88502b
chewei <cheweichan@Cheweide-MacBook-Pro.local>
2019-04-05 23:58:31 +0800

    update

---
+++ 03-fileOperate/fileOperate/nonblock_ synchronous/fifo_write.c
@@ -0,0 +1,36 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#define FIFO_SERVER "./myfifo"
+void main(int argc,char** argv)
+{
+	int fd;
+	char w_buf[100];
+	int nwrite;
+		
+	/*open fifo*/
+	fd=open(FIFO_SERVER,O_WRONLY|O_NONBLOCK,0);
+	
+	if(argc==1)
+	{
+		printf("Please send something\n");
+		exit(-1);
+	}
+	
+	strcpy(w_buf,argv[1]);
+	
+	/* write data to fifo*/
+	if((nwrite=write(fd,w_buf,100))==-1)
+	{
+		if(errno==EAGAIN)
+			printf("The FIFO has not been read yet.Please try later\n");
+	}
+	else 
+		printf("write %s to the FIFO\n",w_buf);
+}
+
